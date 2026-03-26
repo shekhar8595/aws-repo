757089940c5d141b64b47f4245fd170e716bc9d6
shekhar.sharma <shekhar.sharma@inadev.com>
2026-03-26 16:54:55 +0530

    add file

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "myec2" {
   instance_type = "t3.micro"               # ✅ Use t3.micro (NOT t2)
 
   tags = {
-    Name = "Terraform-EC2-new"
+    Name = "Terraform-EC2-new-2"
   }
 }
