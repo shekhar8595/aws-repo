d9c8fb482845c9f8f5956863bcd94cf2416efbbf
shekhar.sharma <shekhar.sharma@inadev.com>
2026-03-26 15:30:12 +0530

    add file

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "myec2" {
+  ami           = "ami-0c02fb55956c7d316"   # Amazon Linux 2 (Free tier safe)
+  instance_type = "t3.micro"               # ✅ Use t3.micro (NOT t2)
+
+  tags = {
+    Name = "Terraform-EC2-new"
+  }
+}
